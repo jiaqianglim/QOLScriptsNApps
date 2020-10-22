adb0af235951228c8658f3eb78eafa93f1b24270
jiaqianglim <32694635+jiaqianglim@users.noreply.github.com>
2020-10-22 17:42:09 +0800

    Update VidtoTopFolder.py

--- File Management/VidtoTopFolder.py
+++ File Management/VidtoTopFolder.py
@@ -1,13 +1,14 @@
 import os, shutil
 
 def object_checker(toplevel, filepath):
+    vidlist4 = ['.mp4', '.wmv', '.avi','.mkv', '.mov', '.flv']
     for i in os.listdir(filepath):
         if os.path.isdir(os.path.join(filepath,i)):
             try:
                 object_checker(toplevel, os.path.join(filepath,i))
             except:
                 pass
-        elif i[-4:] == '.mp4' or i[-4:] == '.wmv' or i[-4:] == '.avi' or i[-4:] == '.mkv' or i[-4:] == '.mov' or i[-4:] == '.flv' or i[-5:] == '.m2ts' or i[-4:] == '.AVI' or i[-4:] == '.MKV' or i[-4:] == '.MP4':
+        elif i[-4:] in vidlist4:
             try:
                 shutil.move(os.path.join(filepath,i), os.path.join(toplevel, i))
             except:
